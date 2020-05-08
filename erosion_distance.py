3c66cb60e470fa1dceb2b644615ee0c80a2092fd
NateClause <nate.clause@gmail.com>
2020-05-08 13:52:53 -0400

    Fixed code breaking in some abnormal cases

--- erosion_distance.py
+++ erosion_distance.py
@@ -1,6 +1,4 @@
 import numpy as np
-import time
-import multiprocessing as mp
 import sys
 
 def erosion(f,tfx,tfy,tfz,g,tgx,tgy,tgz,spacing):
@@ -8,20 +6,25 @@ def erosion(f,tfx,tfy,tfz,g,tgx,tgy,tgz,spacing):
 	newtimes = np.array(nt)
 	newtimes = np.unique(newtimes)
 	newtimes = np.sort(newtimes)
-	n = len(newtimes)	
+	ntx = np.sort(np.unique(np.array(tfx + tgx)))
+	nty = np.sort(np.unique(np.array(tfy + tgy)))
+	ntz = np.sort(np.unique(np.array(tfz + tgz)))
+
+	if tfx != tgx:
+		newf = expand3D(f,tfx,tfy,tfz,ntx,nty,ntz)
+		newg = expand3D(g,tgx,tgy,tgz,ntx,nty,ntz)
+	else:
+		newf = f
+		newg = g
+
+	fshifts = np.zeros((len(ntx),len(nty),len(ntz)), dtype=int)
+	gshifts = np.zeros((len(ntx),len(nty),len(ntz)), dtype=int)
 	
-	newf = expand3D(f,tfx,tfy,tfz,newtimes)
-	newg = expand3D(g,tgx,tgy,tgz,newtimes)
-	
-	fshifts = np.zeros((n,n,n), dtype=int)
-	gshifts = np.zeros((n,n,n), dtype=int)
-	
-	for i in range(n-1,-1,-1):
-		for j in range(n-1,-1,-1):
-			for k in range(n-1,-1,-1):
-				if i!=n-1 and j!=n-1 and k!=n-1 and i+j<n:
-					fshifts[i,j,k]=elementshift(newf,newg,i,j,k,fshifts[i+1,j+1,k+1]+1)
-					gshifts[i,j,k]=elementshift(newg,newf,i,j,k,gshifts[i+1,j+1,k+1]+1)
+	for i in range(len(ntx)):
+		for j in range(len(nty)):
+			for k in range(len(ntz)):
+				fshifts[i,j,k]=elementshift(newf,newg,i,j,k)
+				gshifts[i,j,k]=elementshift(newg,newf,i,j,k)
 		
 	needshifts = np.maximum(fshifts,gshifts)
 	d = np.amax(needshifts) * spacing
@@ -29,53 +32,54 @@ def erosion(f,tfx,tfy,tfz,g,tgx,tgy,tgz,spacing):
 	return d
 
 
-def expand3D(f, tfx, tfy, tfz, nt):
-	n = len(nt)
+def expand3D(f, tfx, tfy, tfz, ntx, nty, ntz):
+	nx = len(ntx)
+	ny = len(nty)
+	nz = len(ntz)
 
 	mx = len(tfx)
 	my = len(tfy)
 	mz = len(tfz)
-	
-	newf = np.zeros((n,n,n), dtype=int)
-	currentz = -1
-	#print(tfz[currentz+1],nt[0])
-	for k in range(n):
-		if currentz < mz-1:
-			if tfz[currentz + 1] == nt[k]:
-				currentz += 1
-				#print(currentz)
-		
-		currenty = -1
-		for i in range(n):
-			if currenty < my-1:
-				if tfy[currenty + 1] == nt[i]:
-					currenty += 1
+	newf = np.zeros((nx, ny, nz), dtype=int)
+	currentx = 0
 
-			currentx = -1
-			for j in range(n):
-				if currentx >= mx-1:
-					newf[j,i,k] = f[mx-1,currenty,currentz]
-					#print(i,j,k,currentz,currenty,mx-1,f[currentz,currenty,mx-1])
-				elif tfx[currentx+1] == nt[j]:
-					currentx +=1
-					newf[j,i,k] = f[currentx,currenty,currentz]
-					#print(i,j,k,currentz,currenty,currentx,f[currentz,currenty,currentx])
+	for k in range(nx):
+		if currentx < mx - 1:
+			while tfx[currentx + 1] <= ntx[k] and currentx < mx - 1:
+				currentx += 1
+				if currentx == mx - 1:
+					break
+
+		currenty = 0
+		for i in range(ny):
+			if currenty < my - 1:
+				while tfy[currenty + 1] <= nty[i] and currenty < my - 1:
+					currenty += 1
+					if currenty == my - 1:
+						break
+
+			currentz = 0
+			for j in range(nz):
+				if currentz >= mz - 1:
+					newf[k, i, j] = f[currentx, currenty, mz - 1]
 				else:
-					newf[j,i,k] = f[currentx,currenty,currentz]
-					#print(i,j,k,currentz,currenty,currentx,f[currentz,currenty,currentx])
+					while tfz[currentz + 1] <= ntz[j] and currentz < mz - 1:
+						currentz += 1
+						if currentz == mz - 1:
+							break
+					newf[k, i, j] = f[currentx, currenty, currentz]
 
 	return newf
 
 
-def elementshift(f,g,i,j,k,old):
+def elementshift(f,g,i,j,k):
 	[i1, j1, k1] = np.shape(f)
-	if old == -1:
-		r = min([i1-i, j, k])
-	else:
-		r = min([i1-i,j,k,old])
-
-	if f[i,j,k] >= g[i,j,k]:
+	r = min([i1-i-1, j-1, k-1])
+	r = max([r, 0])
+	if f[i,j,k] >= g[i,j,k] or f[i,j,k] == 0:
 		return 0
+	if f[i,j,k] < g[i+r,j-r,k-r]:
+		return r+1
 	
 	l = 0
 	current = int(np.floor((r+l)/2))
@@ -123,11 +127,13 @@ if __name__ == "__main__":
 	if spacing1 != spacing2:
 		print("The current algorithm only works when the threshold parameters for the two betti-0 functions have the same spacing. Please re-generate the betti-0 functions so this is the case.")
 		exit()
-	
+	if len(times1) != len(times2):
+		print("The current algorithm only works when the same number of time samples are used for the two betti-0 functions. Please re-generate the betti-0 functions so this is the case.")
+
 	F1 = np.loadtxt(filename1, skiprows=1)
 	F1 = F1.reshape((len(thresholds1),len(times1),len(times1)))
 	F2 = np.loadtxt(filename2, skiprows=1)
 	F2 = F2.reshape((len(thresholds2),len(times2),len(times2)))
 	
-	print(erosion(F1,thresholds1,times1,times1,F2,thresholds2,times2,times2,spacing1))
+	print(erosion(F1,thresholds1,times1,times1,F2,thresholds2,times1,times1,spacing1))
 
